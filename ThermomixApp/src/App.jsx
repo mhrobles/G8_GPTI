eb2930ddb6ad1f023f5bf2ffe1b446388bfa3c1c
mhrobles <mhrobles@uc.cl>
2023-10-18 19:58:12 -0300

    add: avance parcial de API

--- ThermomixApp/src/App.jsx
+++ ThermomixApp/src/App.jsx
@@ -1,10 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [response, setResponse] = useState('')
+  const API_KEY = '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'
+  
+  async function get_res() {
+    const res = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        // Aqui va lo que se le manda a chatGPT, con el rol de usuario y el contenido del mensaje
+        messages:[
+          {"role": "user", "content": "'Lamborgini' es un plato de comida? si o no?"}
+        ],
+        // max_tokens se refiere al largo de la respuesta proveniente de ChatGPT
+        max_tokens: 1,
+        // temperature se refiere a que tan consisa y exacta se quiere tener la respuesta
+        temperature: 0.2
+      })
+    })
+    const data = await res.json()
+    setResponse(data)
+    console.log(data)
+  }
+
+  get_res()
+
+  // Si cambia el valor de response, se ejecuta un console.log
+  useEffect(() => {
+    console.log(response)
+  }, [response])
 
   return (
     <>
